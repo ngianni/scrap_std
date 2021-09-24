61f7c76df732f934cddb08756b211ceb3fb61f95
Nicolás Gianni <48654915+ngianni@users.noreply.github.com>
2021-09-24 17:11:05 -0300

    added login

--- login.js
+++ login.js
@@ -1,35 +1,39 @@
 
+const cheerio= require("cheerio");
+const request= require("request-promise");
+const fs= require("fs-extra");
 
 const puppeteer = require('puppeteer');
 const ACC = require('./acceso');
 const USERNAME_SELECTOR = "#UserName";
 const PASSWORD_SELECTOR = "#UserPassword";
 const CTA_SELECTOR = '#loginButton';
-const SPAN= ".project-description ng-isolate-scope"
+const DIV = "div.project-description"
 
 
 
 async function startBrowser() {
-        // carga navegador
 
-        let browser = await puppeteer.launch({headless: false}); 
-
-        // navegador incognito
-
-        let context = await browser.createIncognitoBrowserContext();
-
-        // abre pagina
-
-        let page = await context.newPage(); 
+  // carga navegador
+  let browser = await puppeteer.launch({      
+    headless: false,
+    defaultViewport: null,
+    args: ['--start-maximized'] 
+  }); 
+  // navegador incognito
+  let context = await browser.createIncognitoBrowserContext();
+  // abre pagina
+  let page = await context.newPage(); 
 
   return {browser, page};
 }
 
-async function closeBrowser(browser) {
-  return browser.close();
-}
+// async function closeBrowser(browser) {
+//   return browser.close();
+// }
 
-async function playTest(url) {
+const playTest = async (url) => {
+
   const {browser, page} = await startBrowser();
   page.setViewport({width: 1366, height: 768});
   await page.goto(url);
@@ -42,22 +46,64 @@ async function playTest(url) {
   await page.click(CTA_SELECTOR);
 
   
-  //await page.waitForNavigation();
-  //await page.waitForSelector();
+  await page.waitForNavigation();
+  await page.waitForSelector('div.project-description')
 
-  //await page.waitForSelector('div.project-description')
+  await page.click(DIV)
 
-  //await page.click(SPAN)
-  //await page.$eval('[class="project-description ng-isolate-scope"]', as => console.log(as) )
+  await page.waitForNavigation();
+  await page.waitForSelector('a[title="STD GCBA"]')
 
-  //await page.click(SPAN)
-  //await page.screenshot({path: 'linkedin.png'});
+  await page.click('a[title="STD GCBA"]')
+
+  await page.waitForSelector('span[content="Analyze in Text iQ"] a.consumption-module-link i')
+  
+  await page.click('span[content="Analyze in Text iQ"] a.consumption-module-link i')
+  
+
+
+  // Reviews
+  await page.waitForSelector('#recordGrid')
+  let contenedor  = await page.$('#recordGrid')
+
+  let subcontenedores = await contenedor.$$('div[ng-repeat="r in records track by $index"]')
+
+  // console.log(subcontenedores.length)
+  // console.log(subcontenedores[[1]])
+
+
+  // let sentimiento = await subcontenedores[0].$eval('div.response-data-container div.record-sentiment-container div.sentiment-diamond', element => element.innerText)
+  // console.log(sentimiento)
+
+  let reviews = []
+// 
+  for (subcontenedor of subcontenedores){
+
+    let comentario = await subcontenedor.$eval('div.response-data-container div.record-container div.response-container span', element => element.innerText)
+    let fecha = await subcontenedor.$eval('div.response-data-container div.response-fields span', element => element.innerText)
+  
+    review = {
+      comentario: comentario,
+      fecha: fecha
+      // sentimiento: ,
+    }
+
+    reviews.push(review)
+
+  } 
+
+  console.log(reviews)
 }
   
-(async () => {
-  await playTest("https://ciudaddebuenosaires.co1.qualtrics.com");
-  //process.exit(1);
-})();
+playTest("https://ciudaddebuenosaires.co1.qualtrics.com");
 
 
-//document.querySelector('span[text="foo"]')
+
+/* 
+
+1. identificar contenedor de comentarios
+2. identificar comentario
+3. extraer: fecha, sentimiento y comentario
+4. navegar por paginas
+
+*/
